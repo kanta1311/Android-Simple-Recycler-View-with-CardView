87b242cecbb914da28a14ac9c61743e4f23aa98c
Arvind Kant <kanta1311@gmail.com>
2017-10-12 11:35:43 +0530

    initial commit

---
+++ app/src/main/java/com/kantapp/recycler_card/MainActivity.java
@@ -0,0 +1,106 @@
+package com.kantapp.recycler_card;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> countries;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager listLayoutmanager=new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(listLayoutmanager);
+        countries=new ArrayList<>();
+        countries.add("Australia");
+        countries.add("India");
+        countries.add("United States of America");
+        countries.add("Germany");
+        countries.add("Russia");
+        RecyclerView.Adapter adapter=new DataAdapter(countries);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+
+            GestureDetector gestureDetector=new GestureDetector(getApplicationContext(),new GestureDetector.SimpleOnGestureListener(){
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+            });
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                View view = rv.findChildViewUnder(e.getX(),e.getY());
+                if(view!=null && gestureDetector.onTouchEvent(e))
+                {
+                    int position=rv.getChildAdapterPosition(view);
+                    Toast.makeText(MainActivity.this, countries.get(position), Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+    }
+}
+
+class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> {
+    private ArrayList<String> countries;
+
+    public DataAdapter(ArrayList<String> countries) {
+        this.countries = countries;
+    }
+
+    @Override
+    public DataAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_row, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DataAdapter.ViewHolder viewHolder, int i) {
+
+        viewHolder.tv_country.setText(countries.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return countries.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView tv_country;
+        public ViewHolder(View view) {
+            super(view);
+
+            tv_country = (TextView)view.findViewById(R.id.tv_country);
+        }
+    }
+
+}
